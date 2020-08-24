913307117b3f2382eafb33a36f0056a33f9851ff
Denis Nazarov <dzianis.nazarau@gmail.com>
2020-08-24 18:52:04 +0300

    fix 8.1 & 8.2

--- src/Ex8_1.java
+++ src/Ex8_1.java
@@ -4,9 +4,7 @@ public class Ex8_1 {
 
     public static void main(String[] args) {
         float distance = 10;
-        for (int i = 2; i < 8; i++) {
-            distance *= 1.1;
-        }
+        for (int i = 2; i < 8; i++, distance *= 1.1);
         System.out.println(distance);
     }
 }
--- src/Ex8_2.java
+++ src/Ex8_2.java
@@ -3,8 +3,7 @@ public class Ex8_2 {
 //    Деление клетки амебы за 24 часа
 
     public static void main(String[] args) {
-        int j = 1;
-        for (int i = 3; i < 25; i+=3) {
+        for (int i = 3, j = 1; i < 25; i+=3) {
             System.out.println("Через " + i + " часа будет " + (j *= 2) + " клеток");
         }
     }
